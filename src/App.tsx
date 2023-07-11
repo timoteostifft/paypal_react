28bee5fef32309ae9b3726df16c501f6f2491fec
timoteostifft <timoteostifft@gmail.com>
2023-07-11 11:59:37 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import { Router } from "./router"
+import { GlobalStyle } from './styles/global'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Router />
+      <GlobalStyle />
     </BrowserRouter>
   )
 }
